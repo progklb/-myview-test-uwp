f0643dc88a93e6b547a71e5ee85eddd79dd205b0
0bl1v1on <kevin.baynham.k7@gmail.com>
2017-08-06 01:30:44 +0200

    [A] Added image provider class to provide images to UI. [A] Added Unsplash image classe that represents a downloaded image. [I] Main page cycles between the two images.

--- MyView/MainPage.xaml.cs
+++ MyView/MainPage.xaml.cs
@@ -22,9 +22,36 @@ namespace MyView
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        ImageProvider m_Provider;
+
+        int m_CurrentImage;
+        Image[] m_Images;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            m_Provider = new ImageProvider();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            m_Images = new Image[] { ImagePrimary, ImageSecondary };
+
+            m_Provider.CycleTime = 3000;
+            m_Provider.Start();
+            m_Provider.OnImageCycled += OnImageCycled;
+        }
+
+        void OnImageCycled(UnsplashImage image)
+        {
+            int oldImage = m_CurrentImage;
+            m_CurrentImage = (++m_CurrentImage % 2);
+
+            m_Images[oldImage].Opacity = 0;
+            m_Images[m_CurrentImage].Opacity = 1;
         }
     }
 }
---
+++ MyView/Source/ImageProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyView
+{
+    class ImageProvider
+    {
+        public event Action<UnsplashImage> OnImageCycled = delegate { };
+
+        public bool Running;
+        public int CycleTime = 5000;
+
+        public void Start()
+        {
+            Running = true;
+
+            StartService();
+        }
+
+        public void Stop()
+        {
+            Running = false;
+        }
+
+        async Task StartService()
+        {
+            while (Running)
+            {
+                OnImageCycled(new UnsplashImage { ImagePath = "example1.jpg" });
+                await Task.Delay(CycleTime);
+                OnImageCycled(new UnsplashImage { ImagePath = "example2.jpg" });
+                await Task.Delay(CycleTime);
+            }
+        }
+    }
+}
